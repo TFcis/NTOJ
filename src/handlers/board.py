95142e36287ead32653c8f14ab93e04f48108c97
tobiichi3227 <cz1346219@gmail.com>
2023-10-02 00:08:02 +0800

    refactor(board): migrate data from redis to db

--- src/handlers/board.py
+++ src/handlers/board.py
@@ -1,11 +1,10 @@
-import os
 import time
 import datetime
 
 import tornado.web
 
 from services.user import UserService, UserConst
-from services.contest import ContestService, ContestConst
+from services.board import BoardService, BoardConst
 from services.pro import ProService
 from services.rate import RateService
 from handlers.base import RequestHandler, reqenv
@@ -13,32 +12,31 @@ from handlers.base import RequestHandler, reqenv
 
 class BoardHandler(RequestHandler):
     @reqenv
-    async def get(self):
-        try:
-            cont_name = str(self.get_argument('cont'))
+    async def get(self, board_id=None):
+        if board_id is None:
+            _, boardlist = await BoardService.inst.get_boardlist()
+            await self.render('board-list', boardlist=boardlist)
+            return
 
-        except tornado.web.HTTPError:
-            cont_name = 'default'
-
-        err, meta = await ContestService.inst.get(cont_name)
+        board_id = int(board_id)
+        err, meta = await BoardService.inst.get_board(board_id)
         if err:
-            self.finish(err)
+            self.error(err)
             return
 
         delta = meta['end'] - datetime.datetime.now().replace(tzinfo=datetime.timezone(datetime.timedelta(hours=8)))
+
         deltasecond = delta.days * 24 * 60 * 60 + delta.seconds
-        cont_list = await ContestService.inst.get_list()
-        boardtempl = 'board'
+        if deltasecond <= 0:
+            # TODO: Cache It
+            pass
+        _, board_list = await BoardService.inst.get_boardlist()
 
-        if self.acct['acct_type'] == UserConst.ACCTTYPE_KERNEL and cont_name != 'default':
-            if os.path.exists(f'/srv/oj/backend/templ/{cont_name}_board.templ'):
-                boardtempl = f'{cont_name}_board'
-
-        if meta['status'] == ContestConst.STATUS_OFFLINE:
+        if meta['status'] == BoardConst.STATUS_OFFLINE:
             self.error('Eacces')
             return
 
-        if (meta['status'] == ContestConst.STATUS_HIDDEN and
+        if (meta['status'] == BoardConst.STATUS_HIDDEN and
                 self.acct['acct_type'] != UserConst.ACCTTYPE_KERNEL):
             self.error('Eacces')
             return
@@ -47,173 +45,82 @@ class BoardHandler(RequestHandler):
         if self.acct['acct_type'] == UserConst.ACCTTYPE_KERNEL:
             min_type = UserConst.ACCTTYPE_KERNEL
 
-        if cont_name == 'default':
-            clas = meta['class']
-            err, prolist = await ProService.inst.list_pro(acct=self.acct, clas=clas)
-            err, acctlist = await UserService.inst.list_acct(min_type=min_type)
-            err, ratemap = await RateService.inst.map_rate(clas=clas, starttime=meta['start'], endtime=meta['end'])
+        # TODO: performance test
+        err, prolist = await ProService.inst.list_pro(acct=self.acct)
+        err, acctlist = await UserService.inst.list_acct(min_type=min_type)
+        err, ratemap = await RateService.inst.map_rate(starttime=meta['start'], endtime=meta['end'])
 
-            # TODO: performance test
-            for acct in acctlist:
-                acct_id = acct['acct_id']
+        prolist2 = []
+        for pro in prolist:
+            if pro['pro_id'] in meta['pro_list']:
+                prolist2.append(pro)
+
+        acctlist2 = []
+        submit_count = {}
+        for acct in acctlist:
+            if acct['acct_id'] in meta['acct_list']:
                 acct['rate'] = 0
-                for pro in prolist:
+                acct_id = acct['acct_id']
+                count = 0
+
+                for pro in prolist2:
                     pro_id = pro['pro_id']
                     if acct_id in ratemap and pro_id in ratemap[acct_id]:
                         rate = ratemap[acct_id][pro_id]
                         acct['rate'] += rate['rate']
 
-            def turn(acct):
-                acct_id = acct['acct_id']
-                count = 0
-                for pro in prolist:
-                    pro_id = pro['pro_id']
-                    if acct_id in ratemap and pro_id in ratemap[acct_id]:
-                        rate = ratemap[acct_id][pro_id]
                         if rate['rate'] > 0:
-                            count = count - rate['count']
+                            count -= rate['count']
+                acctlist2.append(acct)
+                submit_count.update({acct_id: (acct['rate'], count)})
 
-                return acct['rate'], count
+        acctlist2.sort(key=lambda acct: submit_count[acct['acct_id']], reverse=True)
 
-            submit_count = {None: None}
-            for acct in acctlist:
-                acct_id = acct['acct_id']
-                submit_count.update({acct_id: turn(acct)})
-            acctlist.sort(key=lambda acct: submit_count[acct['acct_id']], reverse=True)
+        rank = 0
+        last_sc = None
+        last_sb = None
+        acct_submit = {}
+        for acct in acctlist2:
+            acct_id = acct['acct_id']
+            sc = submit_count[acct_id][0]
+            sb = -submit_count[acct_id][1]
+            acct_submit.update({acct_id: sb})
 
-            acct_submit = {None: None}
-            for acct in acctlist:
-                acct_id = acct['acct_id']
-                acct_submit.update({acct_id: 0})
+            if sc != last_sc:
+                last_sc = sc
+                last_sb = sb
+                rank += 1
 
-            pro_sc_sub = {None: None}
-            for pro in prolist:
-                pro_id = pro['pro_id']
-                sc_add = 0
-                sub_add = 0
-                for acct in acctlist:
-                    acct_id = acct['acct_id']
-                    if acct_id in ratemap and pro_id in ratemap[acct_id]:
-                        rate = ratemap[acct_id][pro_id]
-                        sub_add += rate['count']
-                        sc_add += rate['rate']
-                        if rate['rate'] > 0:
-                            acct_submit[acct_id] += rate['count']
-                pro_sc_sub.update({pro_id: (sc_add, sub_add)})
+            elif sb != last_sb:
+                last_sb = sb
+                rank += 1
 
-            rank = 0
-            last_rate = None
-            last_submit = None
-            for acct in acctlist:
-                submit = submit_count[acct['acct_id']][1]
-                if acct['rate'] != last_rate:
-                    rank += 1
-                    last_rate = acct['rate']
-                    last_submit = submit
+            acct['rank'] = rank
 
-                elif submit != last_submit and last_rate != 0:
-                    rank += 1
-                    last_submit = submit
-
-                acct['rank'] = rank
-
-            await self.render('board',
-                              prolist=prolist,
-                              acctlist=acctlist,
-                              ratemap=ratemap,
-                              pro_sc_sub=pro_sc_sub,
-                              acct_submit=acct_submit,
-                              cont_name=cont_name,
-                              cont_list=cont_list,
-                              end=str(meta['end']).split('+')[0].replace('-', '/'),
-                              timestamp=int(time.time()))
-
-        else:
-
-            # TODO: performance test
-            err, prolist = await ProService.inst.list_pro(acct=self.acct)
-            err, acctlist = await UserService.inst.list_acct(min_type=min_type)
-            err, ratemap = await RateService.inst.map_rate(starttime=meta['start'], endtime=meta['end'])
-
-            prolist2 = []
-            for pro in prolist:
-                if pro['pro_id'] in meta['pro_list']:
-                    prolist2.append(pro)
-
-            acctlist2 = []
-            submit_count = {}
-            for acct in acctlist:
-                if acct['acct_id'] in meta['acct_list']:
-                    acct['rate'] = 0
-                    acct_id = acct['acct_id']
-                    count = 0
-
-                    for pro in prolist2:
-                        pro_id = pro['pro_id']
-                        if acct_id in ratemap and pro_id in ratemap[acct_id]:
-                            rate = ratemap[acct_id][pro_id]
-                            acct['rate'] += rate['rate']
-
-                            if rate['rate'] > 0:
-                                count -= rate['count']
-                    acctlist2.append(acct)
-                    submit_count.update({acct_id: (acct['rate'], count)})
-
-            acctlist2.sort(key=lambda acct: submit_count[acct['acct_id']], reverse=True)
-            # dbg_print(__file__, 301, acctlist2=acctlist2)
-            # dbg_print(__file__, 302, submit_count=submit_count)
-
-            rank = 0
-            last_sc = None
-            last_sb = None
-            acct_submit = {}
+        # NOTE: board最下面的score/submit那行
+        pro_sc_sub = {}
+        for pro in prolist2:
+            pro_id = pro['pro_id']
+            sc = 0
+            sub = 0
             for acct in acctlist2:
                 acct_id = acct['acct_id']
-                sc = submit_count[acct_id][0]
-                sb = -submit_count[acct_id][1]
-                acct_submit.update({acct_id: sb})
+                if acct_id in ratemap and pro_id in ratemap[acct_id]:
+                    rate = ratemap[acct_id][pro_id]
+                    sc += rate['rate']
+                    sub += rate['count']
 
-                if sc != last_sc:
-                    last_sc = sc
-                    last_sb = sb
-                    rank += 1
+            pro_sc_sub.update({pro_id: (sc, sub)})
 
-                elif sb != last_sb:
-                    last_sb = sb
-                    rank += 1
-
-                acct['rank'] = rank
-
-            # dbg_print(__file__, 325, acctlist2=acctlist2)
-            # dbg_print(__file__, 326, acct_submit=acct_submit)
-            # dbg_print(__file__, 327, submit_count=submit_count)
-
-            # INFO: board最下面的score/submit那行
-            pro_sc_sub = {}
-            for pro in prolist2:
-                pro_id = pro['pro_id']
-                sc = 0
-                sub = 0
-                for acct in acctlist2:
-                    acct_id = acct['acct_id']
-                    if acct_id in ratemap and pro_id in ratemap[acct_id]:
-                        rate = ratemap[acct_id][pro_id]
-                        sc += rate['rate']
-                        sub += rate['count']
-
-                pro_sc_sub.update({pro_id: (sc, sub)})
-
-            # dbg_print(__file__, 344, pro_sc_sub=pro_sc_sub)
-
-            await self.render(boardtempl,
-                              prolist=prolist2,
-                              acctlist=acctlist2,
-                              ratemap=ratemap,
-                              pro_sc_sub=pro_sc_sub,
-                              acct_submit=acct_submit,
-                              cont_name=cont_name,
-                              cont_list=cont_list,
-                              end=str(meta['end']).split('+')[0].replace('-', '/'),
-                              timestamp=int(time.time()))
+        await self.render('board',
+                          prolist=prolist2,
+                          acctlist=acctlist2,
+                          ratemap=ratemap,
+                          pro_sc_sub=pro_sc_sub,
+                          acct_submit=acct_submit,
+                          name=meta['name'],
+                          board_list=board_list,
+                          end=str(meta['end']).split('+')[0].replace('-', '/'),
+                          timestamp=int(time.time()))
 
         return
---
+++ src/services/board.py
@@ -0,0 +1,130 @@
+import datetime
+
+from msgpack import packb, unpackb
+
+from services.group import GroupService
+
+
+class BoardConst:
+    STATUS_ONLINE = 0
+    STATUS_HIDDEN = 1
+    STATUS_OFFLINE = 2
+
+
+class BoardService:
+    def __init__(self, db, rs):
+        self.db = db
+        self.rs = rs
+        BoardService.inst = self
+
+    async def get_boardlist(self):
+        async with self.db.acquire() as con:
+            res = await con.fetch('SELECT "board_id", "name", "status", "start", "end" FROM "board";')
+
+        return None, res
+
+    async def get_board(self, board_id):
+        board_id = int(board_id)
+
+        async with self.db.acquire() as con:
+            res = await con.fetchrow('SELECT * FROM "board" WHERE "board_id" = $1', board_id)
+
+            if res.__len__() == 0:
+                return 'Enoext', None
+
+        name, status, start, end, pro_list, acct_list = res['name'], res['status'], res['start'], res['end'], res['pro_list'], res['acct_list']
+
+        meta = {
+            'name': name,
+            'status': status,
+            'pro_list': pro_list,
+            'acct_list': acct_list
+        }
+
+        meta['start'] = start.replace(tzinfo=datetime.timezone(
+            datetime.timedelta(hours=8)))
+        meta['end'] = end.replace(tzinfo=datetime.timezone(
+            datetime.timedelta(hours=8)))
+
+        return None, meta
+
+    async def add_board(self, name, status, start, end, pro_list_str: str, acct_list_str: str):
+        pro_list_str = pro_list_str.replace(' ', '').split(',')
+        pro_list = []
+        for pro in pro_list_str:
+            try:
+                pro_list.append(int(pro))
+            except ValueError:
+                pass
+
+        acct_list_str = acct_list_str.replace(' ', '').split(',')
+        acct_list = []
+        for acct in acct_list_str:
+            if acct != '':
+                if acct.isnumeric():
+                    acct_list.append(int(acct))
+
+                elif acct.find("_group") != -1:
+                    gacct = await GroupService.inst.list_acct_in_group(acct[:-6])
+                    for ga in gacct:
+                        acct_list.append(int(ga['acct_id']))
+
+        pro_list = list(set(pro_list))
+        acct_list = list(set(acct_list))
+
+        async with self.db.acquire() as con:
+            await con.execute(
+                '''
+                    INSERT INTO "board" ("name", "status", "start", "end", "pro_list", "acct_list")
+                    VALUES ($1, $2, $3, $4, $5, $6);
+                ''',
+                name, status, start, end, pro_list, acct_list
+            )
+
+        return None, None
+
+    async def update_board(self, board_id, name, status, start, end, pro_list_str, acct_list_str):
+        board_id = int(board_id)
+
+        pro_list_str = pro_list_str.replace(' ', '').split(',')
+        pro_list = []
+        for pro in pro_list_str:
+            try:
+                pro_list.append(int(pro))
+            except ValueError:
+                pass
+
+        acct_list_str = acct_list_str.replace(' ', '').split(',')
+        acct_list = []
+        for acct in acct_list_str:
+            if acct != '':
+                if acct.isnumeric():
+                    acct_list.append(int(acct))
+
+                elif acct.find("_group") != -1:
+                    gacct = await GroupService.inst.list_acct_in_group(acct[:-6])
+                    for ga in gacct:
+                        acct_list.append(int(ga['acct_id']))
+
+        pro_list = list(set(pro_list))
+        acct_list = list(set(acct_list))
+
+        async with self.db.acquire() as con:
+            res = await con.fetch(
+                '''
+                    UPDATE "board" SET "name" = $1, "status" = $2, "start" = $3, "end" = $4, "pro_list" = $5,
+                    "acct_list" = $6 WHERE "board_id" = $7 RETURNING "board_id";
+                ''',
+                name, status, start, end, pro_list, acct_list, board_id
+            )
+            if res.__len__() != 1:
+                return 'Enoext', None
+
+        return None, None
+
+    async def remove_board(self, board_id):
+        board_id = int(board_id)
+        async with self.db.acquire() as con:
+            await con.execute('DELETE FROM "board" WHERE "board_id" = $1', board_id)
+
+        return None, None
