7ee893154db412f17b0183199262c55d2bc05e18
tobiichi3227 <cz1346219@gmail.com>
2023-10-02 15:01:39 +0800

    fix: can't upload problem

--- src/static/pack.js
+++ src/static/pack.js
@@ -19,44 +19,49 @@ var pack = new function() {
 	    return defer.promise();
     };
     that.send = function(pack_token, file) {
-	// var ws = new WebSocket(`wss://${location.hostname}/oj/be/pack`);
-	var ws = new WebSocket(`ws://${location.hostname}/oj/be/pack`);
-	var defer = $.Deferred();
-	var off = 0;
-	var remain = file.size;
-	var lt = 0;
+        let ws_link = '';
+        if (location.protocol !== 'https:') {
+            ws_link = `ws://${location.hostname}/oj/be/pack`;
+        } else {
+            ws_link = `wss://${location.hostname}/oj/be/pack`;
+        }
+        var ws = new WebSocket(ws_link);
+        var defer = $.Deferred();
+        var off = 0;
+        var remain = file.size;
+        var lt = 0;
 
-	ws.onopen = function(e) {
-	    ws.send(JSON.stringify({
-		    'pack_token' : pack_token,
-		    'pack_size' : file.size
-	    }));
-	};
-	ws.onmessage = function(e) {
-	    var size;
-	    var ct;
+        ws.onopen = function(e) {
+            ws.send(JSON.stringify({
+                'pack_token' : pack_token,
+                'pack_size' : file.size
+            }));
+        };
+        ws.onmessage = function(e) {
+            var size;
+            var ct;
 
-	    if (e.data[0] == 'E') {
-		    ws.close();
-		    defer.reject();
-	    } else if (remain > 0) {
-		    size = Math.min(remain, 65536);
-		    ws.send(file.slice(off, off + size));
+            if (e.data[0] == 'E') {
+                ws.close();
+                defer.reject();
+            } else if (remain > 0) {
+                size = Math.min(remain, 65536);
+                ws.send(file.slice(off, off + size));
 
-		    off += size;
-		    remain -= size;
+                off += size;
+                remain -= size;
 
-		    ct = new Date().getTime();
-			if(ct - lt > 500) {
-			    defer.notify(off / file.size);
-			    lt = ct;
-			}
-	    } else {
-			defer.notify(1);
-			defer.resolve();
-	    }
-	};
+                ct = new Date().getTime();
+                if(ct - lt > 500) {
+                    defer.notify(off / file.size);
+                    lt = ct;
+                }
+            } else {
+                defer.notify(1);
+                defer.resolve();
+            }
+        };
 
-	return defer.promise();
+        return defer.promise();
     };
 };
