7bf4c009f5cea33e003e487b02698d5f96fe705f
tobiichi3227 <cz1346219@gmail.com>
2024-05-24 14:34:16 +0800

    feat: add pagination for problem rank page
    Close #63

--- src/handlers/rank.py
+++ src/handlers/rank.py
@@ -1,13 +1,27 @@
 import datetime
 
+import tornado.web
+
 from handlers.base import RequestHandler, reqenv
 
 
-class RankHandler(RequestHandler):
+class ProRankHandler(RequestHandler):
     @reqenv
     async def get(self, pro_id):
         tz = datetime.timezone(datetime.timedelta(hours=+8))
         pro_id = int(pro_id)
+
+        try:
+            pageoff = int(self.get_argument('pageoff'))
+
+        except tornado.web.HTTPError:
+            pageoff = 0
+
+        try:
+            pagenum = int(self.get_argument('pagenum'))
+
+        except tornado.web.HTTPError:
+            pagenum = 20
 
         async with self.db.acquire() as con:
             result = await con.fetch(
@@ -32,15 +46,34 @@ class RankHandler(RequestHandler):
                 '"challenge"."timestamp" ASC, "challenge"."acct_id" ASC'
                 ') temp '
                 'ORDER BY "runtime" ASC, "memory" ASC,'
-                '"timestamp" ASC, "acct_id" ASC;',
-                self.acct.acct_type,
-                pro_id,
+                '"timestamp" ASC, "acct_id" ASC OFFSET $3 LIMIT $4;',
+                self.acct.acct_type, pro_id, pageoff, pagenum,
             )
 
+            total_cnt = await con.fetch(
+                '''
+                SELECT COUNT(*)
+                FROM (
+                SELECT DISTINCT ON ("challenge"."acct_id")
+                "challenge"."chal_id",
+                FROM "challenge"
+                INNER JOIN "account"
+                ON "challenge"."acct_id"="account"."acct_id"
+                LEFT JOIN "challenge_state"
+                ON "challenge"."chal_id"="challenge_state"."chal_id"
+                WHERE "account"."acct_type">= $1 AND "challenge"."pro_id"= $2
+                AND "challenge_state"."state"=1
+                ) AS temp;
+                ''',
+                self.acct.acct_type, pro_id,
+            )
+            total_cnt = total_cnt[0]['count']
+
         chal_list = []
-        for chal_id, acct_id, timestamp, acct_name, runtime, memory in result:
+        for rank, (chal_id, acct_id, timestamp, acct_name, runtime, memory) in enumerate(result):
             chal_list.append(
                 {
+                    'rank': rank + pageoff + 1,
                     'chal_id': chal_id,
                     'acct_id': acct_id,
                     'acct_name': acct_name,
@@ -50,4 +83,4 @@ class RankHandler(RequestHandler):
                 }
             )
 
-        await self.render('rank', pro_id=pro_id, chal_list=chal_list)
+        await self.render('pro-rank', pro_id=pro_id, chal_list=chal_list, pageoff=pageoff, pagenum=pagenum, total_cnt=total_cnt)
