8de057dfbfd1f09c3faba1aabba837abc250629c
lifeadventurer <108756201+LifeAdventurer@users.noreply.github.com>
2024-10-09 20:23:33 +0800

    Refactor: Improve migration logic and error handling in main function

--- migration/migration.py
+++ migration/migration.py
@@ -1,13 +1,13 @@
-import os
 import asyncio
-import inspect
 import importlib
+import inspect
+import os
 import traceback
 
 import asyncpg
+import config
 from redis import asyncio as aioredis
 
-import config
 
 async def main():
     db_conn = await asyncpg.connect(
@@ -16,22 +16,26 @@ async def main():
     redis_conn = await aioredis.Redis(host='localhost', port=6379, db=config.REDIS_DB)
 
     db_version = None
-    result = await db_conn.fetch('''
+    result = await db_conn.fetch(
+        '''
         SELECT EXISTS (
         SELECT 1
         FROM information_schema.tables
         WHERE table_name = 'db_version'
         ) AS is_exist;
-    ''')
+    '''
+    )
     result = result[0]
 
     if not result['is_exist']:
         # create version table
-        await db_conn.execute('''
+        await db_conn.execute(
+            '''
         CREATE TABLE db_version (
             "version" integer
         );
-        ''')
+        '''
+        )
         await db_conn.execute('INSERT INTO db_version ("version") VALUES (0)')
         db_version = 0
 
@@ -39,30 +43,30 @@ async def main():
         result = await db_conn.fetch("SELECT * FROM db_version")
         db_version = int(result[0]['version'])
 
-    migration_files = (file for file in os.listdir('./') if file.endswith('.py') and file != "migration.py" and file != "config.py")
-    migration_files = sorted(migration_files, key=lambda filename: filename[0:14])  # 0:14 means yyyymmddHHMMSS
+    migration_files = sorted(
+        (file for file in os.listdir('./') if file.endswith('.py') and file not in {"migration.py", "config.py"}),
+        key=lambda filename: filename[:14],  # Sort by timestamp (yyyymmddHHMMSS)
+    )
 
-    for version, filename in enumerate(migration_files):
-        version += 1
+    for version, filename in enumerate(migration_files, start=1):
         if version <= db_version:
             continue
 
         if filename.endswith('.py'):
             module_name = filename[:-3]
-            module = importlib.import_module(f"{module_name}")
+            module = importlib.import_module(module_name)
 
             try:
                 if hasattr(module, 'dochange') and inspect.iscoroutinefunction(module.dochange):
                     await module.dochange(db_conn, redis_conn)
 
             except Exception as e:
-                print(f'run migration file {filename} have error', e)
+                print(f"Error running migration file {filename}: {e}")
                 traceback.print_exc()
-                break
-
-            print(f'run migration file {filename} success')
+                continue
 
         await db_conn.execute('UPDATE db_version SET "version"=$1', version)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
