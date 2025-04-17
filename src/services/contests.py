46adfbfbc549ee9291e8c41d42f9526e7291a8c4
tobiichi3227 <cz1346219@gmail.com>
2025-04-17 22:52:42 +0800

    impr(contest): replace delete and bulk insert with upsert using ON CONFLICT

--- src/services/contests.py
+++ src/services/contests.py
@@ -289,27 +289,16 @@ class ContestService:
                         insert_values.pop(illegal_pro_id)
                         continue
 
-            # TODO: improve update method
             if userlist_updated:
-                await con.execute('DELETE FROM contest_users WHERE contest_id = $1', contest.contest_id)
-                insert_values = {}
                 for acct_id, v in contest.user_list.items():
-                    insert_values[acct_id] = (contest.contest_id, acct_id, int(v['status']))
-
-                while True:
-                    if not insert_values:
-                        break
-
                     try:
-                        await con.executemany(
-                            '''INSERT INTO contest_users ("contest_id", "acct_id", "status")
-                            VALUES ($1, $2, $3)''',
-                            insert_values.values()
-                        )
-                        break
-                    except asyncpg.ForeignKeyViolationError as e:
-                        illegal_acct_id = int(re.search(r'Key \(acct_id\)=\((\d+)\)', e.detail).group(1))
-                        insert_values.pop(illegal_acct_id)
+                        await con.execute('''
+                            INSERT INTO contest_users ("contest_id", "acct_id", "status")
+                            VALUES ($1, $2, $3) ON CONFLICT (contest_id, acct_id) DO UPDATE
+                            SET status = EXCLUDED.status
+                            WHERE contest_users.status != EXCLUDED.status;
+                        ''', contest.contest_id, acct_id, int(v['status']))
+                    except asyncpg.ForeignKeyViolationError:
                         continue
 
         b_contest = pickle.dumps(contest)
