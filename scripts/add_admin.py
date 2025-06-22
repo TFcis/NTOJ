TFcis/NTOJ

commit 4dc8c921a026c629a08c60b95af371a1bc7b41cb

chore: remove legacy (#134)
* chore: remove old group system

* chore: remove old api system

* chore: remove problem lock-list
What the heck is that

scripts/add_admin.py
@@ -20,11 +20,6 @@ class UserConst:
 
     ACCTTYPE_KERNEL = 0
 
-
-class GroupConst:
-    KERNEL_GROUP = 'kernel'
-
-
 async def sign_up(mail, pw, name, db, rs):
     tmp_len = len(mail)
     if tmp_len < UserConst.MAIL_MIN:
@@ -59,14 +54,13 @@ async def sign_up(mail, pw, name, db, rs):
             result = await con.fetch(
                 '''
                     INSERT INTO "account"
-                    ("mail", "password", "name", "acct_type", "group")
+                    ("mail", "password", "name", "acct_type")
                     VALUES ($1, $2, $3, $4, $5) RETURNING "acct_id";
                 ''',
                 mail,
                 base64.b64encode(hpw).decode('utf-8'),
                 name,
                 UserConst.ACCTTYPE_KERNEL,
-                GroupConst.KERNEL_GROUP,
             )
 
     except asyncpg.IntegrityConstraintViolationError:

migration/20250620172100_remove_group_system.py
@@ -1,0 +1,3 @@
+async def dochange(db, _):
+    await db.execute('ALTER TABLE account DROP COLUMN "group";')
+    await db.execute('DROP TABLE "group";')

src/handlers/api.py
@@ -1,62 +1,0 @@
-import json
-
-from handlers.base import RequestHandler, reqenv
-from services.pro import ProService
-from services.rate import RateService
-from services.user import UserService
-
-
-class ApiHandler(RequestHandler):
-    @reqenv
-    async def post(self):
-        reqtype = str(self.get_argument('reqtype'))
-        if reqtype == 'AC':
-            acct_id = int(self.get_argument('acct_id'))
-            err, prolist = await ProService.inst.list_pro(acct=None)
-            err, ratemap = await RateService.inst.map_rate()
-            prolist2 = []
-            for pro in prolist:
-                pro_id = pro['pro_id']
-                if acct_id in ratemap and pro_id in ratemap[acct_id]:
-                    rate = ratemap[acct_id][pro_id]
-                    if rate['rate'] >= 100:
-                        prolist2.append(pro_id)
-            self.finish(str(json.dumps({'ac': prolist2})))
-
-        elif reqtype == 'NA':
-            acct_id = int(self.get_argument('acct_id'))
-            err, acct = await UserService.inst.info_acct(acct_id)
-
-            max_status = ProService.inst.get_acct_limit(acct)
-            async with self.db.acquire() as con:
-                prolist = await con.fetch(
-                    '''
-                        SELECT "pro_id" FROM "problem"
-                        WHERE "status" <= $1
-                        ORDER BY "pro_id" ASC;
-                    ''',
-                    max_status,
-                )
-
-            err, ratemap = await RateService.inst.map_rate_acct(acct=acct)
-            prolist2 = []
-            for pro in prolist:
-                pro_id = pro['pro_id']
-                if (rate := ratemap.get(pro_id)) is not None and rate['rate'] < 100:
-                    prolist2.append(pro_id)
-
-            self.finish(str(json.dumps({'na': prolist2})))
-
-        elif reqtype == 'INFO':
-            acct_id = int(self.get_argument('acct_id'))
-            err, acct = await UserService.inst.info_acct(acct_id)
-            if err:
-                self.finish(err)
-                return
-
-            err, rate = await RateService.inst.get_acct_rate_and_chal_cnt(acct)
-            rate = rate['rate']
-
-            if rate is None:
-                rate = 0
-            self.finish(str(json.dumps({'nick': acct.name, 'score': rate})))

src/handlers/manage/acct.py
@@ -1,7 +1,6 @@
 import tornado
 
 from handlers.base import RequestHandler, reqenv, require_permission
-from services.group import GroupService
 from services.log import LogService
 from services.user import UserConst, UserService
 
@@ -26,9 +25,7 @@ class ManageAcctHandler(RequestHandler):
             acct_id = int(self.get_argument('acctid'))
 
             _, acct = await UserService.inst.info_acct(acct_id)
-            glist = await GroupService.inst.list_group()
-            group = await GroupService.inst.group_of_acct(acct_id)
-            await self.render('manage/acct/update', page='acct', acct=acct, glist=glist, group=group)
+            await self.render('manage/acct/update', page='acct', acct=acct)
 
     @reqenv
     @require_permission(UserConst.ACCTTYPE_KERNEL)
@@ -38,7 +35,6 @@ class ManageAcctHandler(RequestHandler):
         if page == 'update' and reqtype == 'update':
             acct_id = int(self.get_argument('acct_id'))
             acct_type = int(self.get_argument('acct_type'))
-            group = str(self.get_argument('group'))
             err, acct = await UserService.inst.info_acct(acct_id)
 
             if err:
@@ -58,5 +54,4 @@ class ManageAcctHandler(RequestHandler):
             if err:
                 return self.error(err)
 
-            _ = await GroupService.inst.set_acct_group(acct_id, group)
             self.error(('S', ''))

src/handlers/manage/board.py
@@ -2,7 +2,6 @@ import datetime
 
 from handlers.base import RequestHandler, reqenv, require_permission
 from services.board import BoardService
-from services.group import GroupService
 from services.log import LogService
 from services.user import UserConst
 
@@ -129,12 +128,6 @@ class ManageBoardHandler(RequestHandler):
             if acct != '':
                 if acct.isnumeric():
                     res.append(int(acct))
-
-                elif acct.find("_group") != -1:
-                    gacct = await GroupService.inst.list_acct_in_group(acct[:-6])
-                    for ga in gacct:
-                        res.append(ga['acct_id'])
-
         return res
 
     def _get_pro_list(self, pro_list_str: str) -> list[int]:

src/handlers/manage/group.py
@@ -1,82 +1,0 @@
-import tornado.web
-
-from handlers.base import RequestHandler, reqenv, require_permission
-from services.group import GroupConst, GroupService
-from services.log import LogService
-from services.user import UserConst
-
-
-class ManageGroupHandler(RequestHandler):
-    @reqenv
-    @require_permission(UserConst.ACCTTYPE_KERNEL)
-    async def get(self):
-        try:
-            gname = str(self.get_argument('gname'))
-
-            async with self.db.acquire() as con:
-                result = await con.fetchrow(
-                    '''
-                        SELECT "group"."group_type", "group"."group_class"
-                        FROM "group"
-                        WHERE "group"."group_name" = $1
-                    ''',
-                    gname,
-                )
-            gtype = int(result['group_type'])
-            gclas = int(result['group_class'])
-
-        except tornado.web.HTTPError:
-            gname = None
-            gtype = None
-            gclas = None
-
-        glist = await GroupService.inst.list_group()
-        if gname is not None:
-            gacct = await GroupService.inst.list_acct_in_group(gname)
-        else:
-            gacct = None
-
-        await self.render('manage/group', page='group', gname=gname, glist=glist, gacct=gacct, gtype=gtype, gclas=gclas)
-
-    @reqenv
-    @require_permission(UserConst.ACCTTYPE_KERNEL)
-    async def post(self):
-        reqtype = str(self.get_argument('reqtype'))
-        if reqtype == 'edit':
-            gname = str(self.get_argument('gname'))
-            gtype = int(self.get_argument('gtype'))
-            gclas = int(self.get_argument('gclas'))
-            if gname == GroupConst.KERNEL_GROUP:
-                return self.error(('Ekernel', 'Cannot edit kernel group'))
-
-            await LogService.inst.add_log(
-                f"{self.acct.name} updated group={gname} group_type={gtype} group_class={gclas}.", 'manage.group.update'
-            )
-            if err := await GroupService.inst.update_group(gname, gtype, gclas):
-                return self.error(err)
-
-            self.error(('S', ''))
-
-        elif reqtype == 'add_group':
-            gname = str(self.get_argument('gname'))
-            gtype = int(self.get_argument('gtype'))
-            gclas = int(self.get_argument('gclas'))
-
-            await LogService.inst.add_log(
-                f"{self.acct.name} added group={gname} group_type={gtype} group_class={gclas}.", 'manage.group.add'
-            )
-            if err := await GroupService.inst.add_group(gname, gtype, gclas):
-                return self.error(err)
-
-            self.error(('S', ''))
-
-        elif reqtype == 'del_group':
-            gname = str(self.get_argument('gname'))
-            if gname in [GroupConst.KERNEL_GROUP, GroupConst.DEFAULT_GROUP]:
-                return self.error(('Ekernel', 'Cannot remove kernel and default group'))
-
-            await LogService.inst.add_log(f"{self.acct.name} deleted group={gname}", 'manage.group.delete')
-            if err := await GroupService.inst.del_group(gname):
-                return self.error(err)
-
-            self.error(('S', ''))

src/handlers/manage/pro.py
@@ -33,12 +33,7 @@ class ManageProHandler(RequestHandler):
             pro_total_cnt = len(prolist)
             prolist = prolist[pageoff: pageoff + 40]
 
-            if (lock_list := (await self.rs.get('lock_list'))) is not None:
-                lock_list = unpackb(lock_list)
-            else:
-                lock_list = []
-
-            await self.render('manage/pro/pro-list', page='pro', prolist=prolist, lock_list=lock_list,
+            await self.render('manage/pro/pro-list', page='pro', prolist=prolist,
                             pageoff=pageoff, pro_total_cnt=pro_total_cnt)
 
         elif page == "update":
@@ -48,10 +43,8 @@ class ManageProHandler(RequestHandler):
             if err:
                 return self.error(err)
 
-            lock = await self.rs.get(f"{pro['pro_id']}_owner")
-
             await self.render(
-                'manage/pro/update', page='pro', pro=pro, lock=lock
+                'manage/pro/update', page='pro', pro=pro
             )
 
         elif page == "add":
@@ -873,34 +866,6 @@ class ManageProHandler(RequestHandler):
                     }
                 )
 
-                self.error(('S', ''))
-
-            elif reqtype == 'pro-lock':
-                pro_id = int(self.get_argument('pro_id'))
-                await self.rs.set(f'{pro_id}_owner', packb(1))
-
-                if (lock_list := (await self.rs.get('lock_list'))) is not None:
-                    lock_list = unpackb(lock_list)
-                else:
-                    lock_list = []
-
-                if pro_id not in lock_list:
-                    lock_list.append(pro_id)
-
-                await self.rs.set('lock_list', packb(lock_list))
-                self.error(('S', ''))
-
-            elif reqtype == 'pro-unlock':
-                pro_id = int(self.get_argument('pro_id'))
-                pwd = str(self.get_argument('pwd'))
-
-                if config.unlock_pwd != base64.b64encode(packb(pwd)):
-                    return self.error(('Eacces', 'Wrong password'))
-
-                lock_list = unpackb((await self.rs.get('lock_list')))
-                lock_list.remove(pro_id)
-                await self.rs.set('lock_list', packb(lock_list))
-                await self.rs.delete(f"{pro_id}_owner")
                 self.error(('S', ''))
 
         elif page is None:  # pro-list

src/handlers/manage/url.py
@@ -3,7 +3,6 @@ from handlers.manage.board import ManageBoardHandler
 from handlers.manage.bulletin import ManageBulletinHandler
 from handlers.manage.contest import ManageContestHandler
 from handlers.manage.dash import ManageDashHandler
-from handlers.manage.group import ManageGroupHandler
 from handlers.manage.judge import JudgeChalCntSub, ManageJudgeHandler
 from handlers.manage.pack import ManagePackHandler
 from handlers.manage.pro import ManageProHandler
@@ -34,7 +33,6 @@ def get_manage_url(db, rs, pool):
         ('/manage/proclass/(.+)', ManageProClassHandler, args),
         ('/manage/question', ManageQuestionHandler, args),
         ('/manage/question/(.+)', ManageQuestionHandler, args),
-        ('/manage/group', ManageGroupHandler, args),
         ('/manage/judge', ManageJudgeHandler, args),
         ('/manage/judgecntws', JudgeChalCntSub, sub_args),
         ('/manage/pack', ManagePackHandler, args),

commit b53d8e5dcfd4b402973409c3134de80492dbbbeb

fix(install): regression after remove legacy group system Regression was introduced in 4dc8c92 (chore: remove legacy)

scripts/add_admin.py
@@ -55,7 +55,7 @@ async def sign_up(mail, pw, name, db, rs):
                 '''
                     INSERT INTO "account"
                     ("mail", "password", "name", "acct_type")
-                    VALUES ($1, $2, $3, $4, $5) RETURNING "acct_id";
+                    VALUES ($1, $2, $3, $4) RETURNING "acct_id";
                 ''',
                 mail,
                 base64.b64encode(hpw).decode('utf-8'),