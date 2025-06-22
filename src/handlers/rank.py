TFcis/NTOJ

commit ea6e7270942881e344803fb536b2b7ad48c42e39

fix(rank): prevent user to hidden problem (#136)
* fix(rank): prevent user to hidden problem
Close #135

* test(rank): add test for spec implement

---------

Co-authored-by: tobiichi3227 <cz1346219@gmail.com>

src/handlers/rank.py
@@ -5,14 +5,22 @@ import tornado.web
 from handlers.base import RequestHandler, reqenv
 from services.user import UserConst, UserService, Account
 from services.chal import ChalConst
+from services.pro import ProConst, ProService
 
+PERMISSION_DENIED_ERROR = (('Eacces', 'Permission denied'))
 
 class ProRankHandler(RequestHandler):
     @reqenv
     async def get(self, pro_id):
         tz = datetime.timezone(datetime.timedelta(hours=+8))
         pro_id = int(pro_id)
+        err, pro = await ProService.inst.get_pro(pro_id, self.acct, is_contest=self.contest is not None)
+        if err:
+            return self.error(err)
 
+        if pro['status'] in [ProConst.STATUS_OFFLINE,ProConst.STATUS_CONTEST]:
+            return self.error(PERMISSION_DENIED_ERROR)
+        
         try:
             pageoff = int(self.get_argument('pageoff'))
 

src/tests/e2e/rank.py
@@ -1,4 +1,5 @@
 from tests.e2e.util import AsyncTest, AccountContext
+from services.pro import ProConst
 
 
 class UserRankTest(AsyncTest):
@@ -20,3 +21,31 @@ class ProRankTest(AsyncTest):
             first = html.select_one('tbody > tr')
             self.assertEqual(first.attrs['class'][0], 'rank-gold')
             self.assertEqual(first.select('td')[2].text, 'admin')
+
+            admin_session.post('manage/pro/update', data={
+                'reqtype': 'updatepro',
+                'pro_id': 1,
+                'name': 'GCD',
+                'status': ProConst.STATUS_HIDDEN,
+                'tags': '',
+                'allow_submit': 'true',
+                'is_makefile': 'false',
+                'check_type': ProConst.CHECKER_DIFF,
+                "rate_precision": ProConst.RATE_PRECISION_MIN,
+            })
+
+            with AccountContext('test1@test', 'test') as user_session:
+                res = user_session.get('rank/1')
+                self.assertAPIReturnValue(res.text, ('Eacces', 'Permission denied'))
+
+            admin_session.post('manage/pro/update', data={
+                'reqtype': 'updatepro',
+                'pro_id': 1,
+                'name': 'GCD',
+                'status': ProConst.STATUS_ONLINE,
+                'tags': '',
+                'allow_submit': 'true',
+                'is_makefile': 'false',
+                'check_type': ProConst.CHECKER_DIFF,
+                "rate_precision": ProConst.RATE_PRECISION_MIN,
+            })

commit dd352447b5a20d007489d03c0b1657be8de55f10

fix(rank): regression after remove unnecessary problem status `OFFLINE` Regression was introduced in ea6e727(fix(rank): prevent user to hidden problem).

src/handlers/rank.py
@@ -18,9 +18,9 @@ class ProRankHandler(RequestHandler):
         if err:
             return self.error(err)
 
-        if pro['status'] in [ProConst.STATUS_OFFLINE,ProConst.STATUS_CONTEST]:
+        if pro['status'] == ProConst.STATUS_CONTEST:
             return self.error(PERMISSION_DENIED_ERROR)
-        
+
         try:
             pageoff = int(self.get_argument('pageoff'))
 

src/tests/e2e/rank.py
@@ -36,7 +36,7 @@ class ProRankTest(AsyncTest):
 
             with AccountContext('test1@test', 'test') as user_session:
                 res = user_session.get('rank/1')
-                self.assertAPIReturnValue(res.text, ('Eacces', 'Permission denied'))
+                self.assertAPIReturnValue(res.text, ('Enoext', 'Problem not found'))
 
             admin_session.post('manage/pro/update', data={
                 'reqtype': 'updatepro',