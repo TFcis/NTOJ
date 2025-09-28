a7ca05c9362ef15c4c07e91914602ade8a3bc04b
tobiichi3227 <cz1346219@gmail.com>
2025-09-28 09:22:26 +0800

    chore(judge): remove unused parentheses

--- src/services/judge.py
+++ src/services/judge.py
@@ -126,7 +126,7 @@ class JudgeServerService:
             total_result["time"] = total_result["time"] // 10 ** 6
             total_result["memory"] = total_result["memory"] // 1024
 
-            await (ChalService.inst.update_total_result(
+            await ChalService.inst.update_total_result(
                 chal_id,
                 TotalResult(
                     total_result["status"],
@@ -136,12 +136,12 @@ class JudgeServerService:
                     message,
                     total_result["message_type"],
                 ),
-            ))
+            )
 
             for subtask_id, subtask_result in result["subtask_results"].items():
                 subtask_result["time"] = subtask_result["time"] // 10 ** 6
                 subtask_result["memory"] = subtask_result["memory"] // 1024
-                await (ChalService.inst.update_subtask_result(
+                await ChalService.inst.update_subtask_result(
                     chal_id,
                     SubtaskResult(
                         int(subtask_id),
@@ -150,12 +150,12 @@ class JudgeServerService:
                         subtask_result["memory"],
                         decimal.Decimal(subtask_result["score"]),
                     ),
-                ))
+                )
 
             for testdata_result in result["testdata_results"].values():
                 testdata_result["time"] = testdata_result["time"] // 10 ** 6
                 testdata_result["memory"] = testdata_result["memory"] // 1024
-                await (ChalService.inst.update_testdata_result(
+                await ChalService.inst.update_testdata_result(
                     chal_id,
                     TestdataResult(
                         testdata_result["id"],
@@ -165,7 +165,7 @@ class JudgeServerService:
                         testdata_result["message"],
                         MessageType(testdata_result["message_type"]),
                     ),
-                ))
+                )
 
             self.running_chal_cnt -= 1
             await self.rs.publish(
