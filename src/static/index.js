973265128724015a7465efce6f2f31bfa5f5b88d
tobiichi3227 <cz1346219@gmail.com>
2026-01-13 09:42:45 +0800

    fix: prevent users from clicking account link when not logged in

--- src/static/index.js
+++ src/static/index.js
@@ -250,8 +250,10 @@ var index = new function() {
         if (acct_id != '0') {
             that.acct_id = parseInt(acct_id);
             j_navlist.find('li.leave').show();
+            j_navlist.find('li.account').show();
         } else {
             j_navlist.find('li.sign').show();
+            j_navlist.find('li.account').hide();
         }
 
         update(false);
