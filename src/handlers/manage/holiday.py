2f11f514a65fe37fec082ba9b99def34d0c5995a
yushiuan9499 <yushiuan9499@gmail.com>
2026-01-21 13:39:40 +0800

    feat(holiday): get holiday&weekday range from backend

--- src/handlers/manage/holiday.py
+++ src/handlers/manage/holiday.py
@@ -1,5 +1,8 @@
+import datetime
+
 from handlers.base import ActionDispatcher, RequestHandler, reqenv, require_permission
 from services.user import UserConst
+from services.holiday import HolidayService, DayRange
 
 holiday_dispatcher = ActionDispatcher()
 
@@ -7,5 +10,36 @@ class ManageHolidayHandler(RequestHandler):
     @reqenv
     @require_permission(UserConst.ACCTTYPE_KERNEL)
     async def get(self):
+        if self.get_argument("action", None) == "events":
+            await self._get_events()
+            return
         await self.render("manage/holiday", page="holiday")
 
+    async def _get_events(self):
+        '''
+            Return json data of the requested month
+        '''
+        year = int(self.get_argument("year"))
+        month = int(self.get_argument("month"))
+        query_range = DayRange(
+            datetime.datetime(year,month,1) - datetime.timedelta(days=7), 
+            datetime.datetime(year,month,1) + datetime.timedelta(days=41)
+        )
+        err, res = await HolidayService.inst.get_days(query_range)
+        if err:
+            return self.error(err)
+
+        RED = '#ff5555'
+        GREEN = '#50fa7b'
+        events = [
+            {
+                'calendarId': str(day['range'].start.timestamp()),
+                'title': f'{day['range'].start.strftime("%H:%M")} - {day['range'].end.strftime("%H:%M")}',
+                'start': day['range'].start.isoformat(),
+                'end': day['range'].end.isoformat(),
+                'backgroundColor': GREEN if day['is_weekday'] else RED,
+                
+            }
+            for day in res
+        ]
+        self.error(('S',events))
