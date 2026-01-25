7aa25dddc46d13c1ec2e024d2d60db1a6ca13a0e
yushiuan9499 <yushiuan9499@gmail.com>
2026-01-25 14:28:39 +0800

    test(holiday): add unit test for redis

--- src/tests/unit/services/test_holiday.py
+++ src/tests/unit/services/test_holiday.py
@@ -18,6 +18,7 @@ class TestHolidayService(unittest.IsolatedAsyncioTestCase):
         self.fake_db.acquire = MagicMock(return_value=fake_acquire_cm)
         self.fake_rs = AsyncMock()
         self.fake_rs.get = AsyncMock(return_value=None)
+        self.fake_rs.set = AsyncMock()
 
         self.service = HolidayService(self.fake_db, rs=self.fake_rs)
 
@@ -28,6 +29,10 @@ class TestHolidayService(unittest.IsolatedAsyncioTestCase):
             res = await self.service.is_weekday_now()
         self.fake_conn.fetchrow.assert_awaited_once()
         self.assertTrue(res)
+        calls = self.fake_rs.set.await_args_list
+        self.assertEqual(len(calls), 2) # set valid time and the result
+        self.assertEqual(calls[0][0][1], 1769118100) # valid time
+        self.assertEqual(calls[1][0][1], 1)       # result
 
     async def test_start_eq_now(self):
         self.fake_conn.fetchrow.return_value = {"start": 1769118000, "end": 1769118100}
@@ -36,6 +41,10 @@ class TestHolidayService(unittest.IsolatedAsyncioTestCase):
             res = await self.service.is_weekday_now()
         self.fake_conn.fetchrow.assert_awaited_once()
         self.assertTrue(res)
+        calls = self.fake_rs.set.await_args_list
+        self.assertEqual(len(calls), 2) # set valid time and the result
+        self.assertEqual(calls[0][0][1], 1769118100) # valid time
+        self.assertEqual(calls[1][0][1], 1)       # result
 
     async def test_start_gt_now(self):
         self.fake_conn.fetchrow.return_value = {"start": 1769118000, "end": 1769118100}
@@ -44,15 +53,46 @@ class TestHolidayService(unittest.IsolatedAsyncioTestCase):
             res = await self.service.is_weekday_now()
         self.fake_conn.fetchrow.assert_awaited_once()
         self.assertFalse(res)
+        calls = self.fake_rs.set.await_args_list
+        self.assertEqual(len(calls), 2) # set valid time and the result
+        self.assertEqual(calls[0][0][1], 1769118000) # valid time
+        self.assertEqual(calls[1][0][1], 0)       # result
 
     async def test_no_row(self):
         self.fake_conn.fetchrow.return_value = None
         res = await self.service.is_weekday_now()
         self.fake_conn.fetchrow.assert_awaited_once()
         self.assertFalse(res)
+        calls = self.fake_rs.set.await_args_list
+        self.assertEqual(len(calls), 2) # set valid time and the result
+        self.assertEqual(calls[1][0][1], 0)       # result
 
     async def test_no_val(self):
         self.fake_conn.fetchrow.return_value = {}
         res = await self.service.is_weekday_now()
         self.fake_conn.fetchrow.assert_awaited_once()
         self.assertFalse(res)
+        calls = self.fake_rs.set.await_args_list
+        self.assertEqual(len(calls), 2) # set valid time and the result
+        self.assertEqual(calls[1][0][1], 0)       # result
+
+    async def test_cache_hit(self):
+        self.fake_rs.get.side_effect = [b'1769118100', b'1']
+        with patch('datetime.datetime') as mock_now:
+            mock_now.now.return_value.timestamp.return_value = 1769118050
+            res = await self.service.is_weekday_now()
+        self.fake_conn.fetchrow.assert_not_awaited()
+        self.assertTrue(res)
+
+    async def test_cache_expired(self):
+        self.fake_rs.get.side_effect = [b'1769117000', b'1']
+        self.fake_conn.fetchrow.return_value = {"start": 1769118000, "end": 1769118100}
+        with patch('datetime.datetime') as mock_now:
+            mock_now.now.return_value.timestamp.return_value = 1769117050
+            res = await self.service.is_weekday_now()
+        self.fake_conn.fetchrow.assert_awaited_once()
+        self.assertFalse(res)
+        calls = self.fake_rs.set.await_args_list
+        self.assertEqual(len(calls), 2) # set valid time and the result
+        self.assertEqual(calls[0][0][1], 1769118000) # valid time
+        self.assertEqual(calls[1][0][1], 0)       # result
