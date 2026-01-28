5b5a5b1cc36afdc0b548a3703c73e06886050c8e
tobiichi3227 <cz1346219@gmail.com>
2026-01-28 21:54:47 +0800

    chore(contest): make code more readable

--- src/handlers/contests/proset.py
+++ src/handlers/contests/proset.py
@@ -62,35 +62,34 @@ class ContestProsetHandler(RequestHandler):
         elif self.contest.is_running() and not self.contest.is_member(self.acct):
             return self.error(PERMISSION_DENIED_ERROR)
 
-        else:
-            if self.contest.contest_mode == ContestMode.RANDOM_SET and not self.contest.is_admin(self.acct):
-                return await self.randomset_proset(pageoff)
+        elif self.contest.contest_mode == ContestMode.RANDOM_SET and not self.contest.is_admin(self.acct):
+            return await self.randomset_proset(pageoff)
 
-            _, acct_rates = await RateService.inst.map_rate_acct(self.acct, contest_id=self.contest.contest_id)
-            _, prolist = await ProService.inst.list_pro(ProConst.PRO_STATUS_CONTEST_USER)
+        _, acct_rates = await RateService.inst.map_rate_acct(self.acct, contest_id=self.contest.contest_id)
+        _, prolist = await ProService.inst.list_pro(ProConst.PRO_STATUS_CONTEST_USER)
 
-            prolist_order = {pro_id: idx for idx, pro_id in enumerate(self.contest.pro_list.keys())}
-            prolist = sorted(filter(lambda pro: self.contest.is_pro(pro.pro_id), prolist),
-                                  key=lambda pro: prolist_order[pro.pro_id])
+        prolist_order = {pro_id: idx for idx, pro_id in enumerate(self.contest.pro_list.keys())}
+        prolist = sorted(filter(lambda pro: self.contest.is_pro(pro.pro_id), prolist),
+                                key=lambda pro: prolist_order[pro.pro_id])
 
-            score_map: dict[int, dict] = {}
+        score_map: dict[int, dict] = {}
+        for pro in prolist:
+            pro_id = pro.pro_id
+            score_map[pro_id] = {'score': 0, 'state': None}
+            if pro_id in acct_rates:
+                score_map[pro_id]['score'] += acct_rates[pro.pro_id]['rate']
+                score_map[pro_id]['state'] = acct_rates[pro.pro_id]['state']
+
+        if self.contest.is_public_scoreboard or self.contest.is_admin(self.acct):
+            show_ac_ratio = True
             for pro in prolist:
-                pro_id = pro.pro_id
-                score_map[pro_id] = {'score': 0, 'state': None}
-                if pro_id in acct_rates:
-                    score_map[pro_id]['score'] += acct_rates[pro.pro_id]['rate']
-                    score_map[pro_id]['state'] = acct_rates[pro.pro_id]['state']
+                _, rate = await RateService.inst.get_pro_ac_rate(pro.pro_id, contest_id=self.contest.contest_id)
+                score_map[pro.pro_id]['rate_data'] = rate
 
-            if self.contest.is_public_scoreboard or self.contest.is_admin(self.acct):
-                show_ac_ratio = True
-                for pro in prolist:
-                    _, rate = await RateService.inst.get_pro_ac_rate(pro.pro_id, contest_id=self.contest.contest_id)
-                    score_map[pro.pro_id]['rate_data'] = rate
-
-            if self.contest.contest_mode == ContestMode.RANDOM_SET:
-                pro_idx_map = {pro_id: idx for idx, pro_set in enumerate(self.contest.pro_sets) for pro_id in pro_set}
-            else:
-                pro_idx_map = {pro_id: idx for idx, pro_id in enumerate(self.contest.pro_list.keys())}
+        if self.contest.contest_mode == ContestMode.RANDOM_SET:
+            pro_idx_map = {pro_id: idx for idx, pro_set in enumerate(self.contest.pro_sets) for pro_id in pro_set}
+        else:
+            pro_idx_map = {pro_id: idx for idx, pro_id in enumerate(self.contest.pro_list.keys())}
 
         pro_total_cnt = len(prolist)
         prolist = prolist[pageoff: pageoff + 40]
